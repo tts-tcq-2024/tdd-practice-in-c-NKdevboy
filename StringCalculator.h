d8ecaa539ce29e98a3c47e13ce48fdf5fa5fbb4d
NKdevboy <nithishkumarramup@gmail.com>
2024-07-11 21:37:41 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,9 +1,27 @@
-int add(const char *input)
+
+int nullCheck(const char *input)
 {
-int returnval = -1;
-  if(*input == '0'||input !=NULL)
+ // if(*input == '0'||input !=NULL)  need to write 
+  if(*input == '0')
   {
     returnval = 0;
   }
+  else
+  {
+    returnval = -1;
+  }
+}
+
+
+
+
+int add(const char *input)
+{
+  int returnval = -1;
+  int Str_len = 0;
+  Str_len = strlen(input);
+  returnval = nullCheck(input)
+
+
   return returnval;
 }
