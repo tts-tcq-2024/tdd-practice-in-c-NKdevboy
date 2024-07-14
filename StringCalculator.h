95991f58dc101bfcb9b6f4da1b517d79fba50d2e
NKdevboy <nithishkumarramup@gmail.com>
2024-07-14 18:02:18 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,3 +1,10 @@
+char ListSep[3] = {",","\n",","};
+
+int outIntArr[1000] ={0};
+int NumListSize[100] = {0};
+int readArrStart =0;
+int TotalNum =0;
+const int TensArr[4]={1,10,100,1000};
 int CharToInt(const char CutChar)
 {
    int value = 0xff;
@@ -37,11 +44,83 @@ int IslengthLessThanOne(const char *inStr,int StrLen,int *OutVal)
     return 0;
   }
 }
+void IsUserdefineSeperatorAvl(const char *inStr)
+{
+   if((inStr[0] == '/')&&(inStr[1] == '/')&&(inStr[3] == '\n'))
+   {
+      ListSep[2] = inStr[2];
+      readArrStart = 4;
+   }
+}
+
+void CopylocArrToGArr(int*LArr ,int numDigit)
+{
+   for(int i = 0;i<numDigit;i++)
+   {
+      outIntArr[((NumListSize[TotalNum])+i)] = LArr[i];
+   }
+   NumListSize[TotalNum] = numDigit;
+   TotalNum++;
+}
+int IsourSep(char Sep)
+{
+   if((ListSep[0] == Sep)||(ListSep[1] == Sep)||(ListSep[2] == Sep))
+   {
+      return 1;
+   }
+   return 0;
+}
+void ReadCharArrToint(const char *inStr)
+{
+   int i=0,j=0,loclArr[4]={0},localNum = 0xff;
+   inStr = inStr+readArrStart;
+   for(;inStr[i] != NULL;i++,j++)
+   {
+      localNum = CharToInt(inStr[i];
+      if((0xff != localNum)&&(j<4))
+      {
+         loclArr[j] = localNum;
+      }
+      else if((j <= 4)&&(IsourSep(inStr[i])))
+      {
+      CopylocArrToGArr(loclArr,j);
+      }
+   }
+}
+int ArrToNum(int cnt)
+{
+   
+   int sum =0,loc =0;
+   for(int i=0;i< cnt;i++)
+   {
+    loc = (outIntArr[i] * TensArr[(cnt-(i+1)]);
+    if(loc =< 1000)
+    {
+       sum +=loc;
+    }
+   }
+   lastidex += cnt;
+}
+
+int addFromGarr(void)
+{
+   /*
+int outIntArr[1000] ={0};
+int NumListSize[100] = {0};
+int TotalNum =0;
+   */
+int outSum =0;
+for(int curntNum =0;TotalNum!=curntNum;,curntNum++)
+{
+   outSum += ArrToNum(NumListSize[curntNum]); 
+}
+}
 
 int AdditionMain(const char *inStr)
 {
-
-   
+   IsUserdefineSeperatorAvl(inStr);
+   ReadCharArrToint(inStr);
+   return addFromGarr();
 }
 int NoNumCharInStr(const char * input)
 {
@@ -65,7 +144,6 @@ int add(const char *input)
   }
   else
   {
-     AdditionMain(input);
-     return 3;
+    return AdditionMain(input);
   }
 }
